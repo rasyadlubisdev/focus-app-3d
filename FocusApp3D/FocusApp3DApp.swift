2b75f49b70f1b5e8799b52e15ccaf934b9ce9ee5
Rasyad Lubis <rasyad>
2026-01-16 16:12:54 +0700

    Initial Commit

---
+++ FocusApp3D/FocusApp3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusApp3DApp.swift
+//  FocusApp3D
+//
+//  Created by Muhammad Rasyad Lubis on 16/01/26.
+//
+
+import SwiftUI
+
+@main
+struct FocusApp3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FocusApp3DTests/FocusApp3DTests.swift
@@ -0,0 +1,17 @@
+//
+//  FocusApp3DTests.swift
+//  FocusApp3DTests
+//
+//  Created by Muhammad Rasyad Lubis on 16/01/26.
+//
+
+import Testing
+@testable import FocusApp3D
+
+struct FocusApp3DTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
